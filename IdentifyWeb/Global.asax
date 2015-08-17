e24fe998953da75f884f13a8e795a7e70c9f6541
me1986au <me_1986au@hotmail.com>
2015-08-17 16:21:45 +0800

    First check in

---
+++ IdentifyWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentifyWeb.MvcApplication" Language="C#" %>
---
+++ IdentifyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentifyWeb.Startup))]
+namespace IdentifyWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
